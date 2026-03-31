05fca3b4b7ad1ffa198be3efbd2e3719187c84c0
SAJNUS <sajnussaharearhojayfa@gmail.com>
2026-03-31 20:30:29 +0600

    ms 3 cs 1.1

---
+++ Milestone 3/Conceptual 1/datatype.js
@@ -0,0 +1,16 @@
+/**
+ * 2 data types
+ * 1. primitive (string, number, boolean, undefined, null)
+ * 2. non-primitive or referential (array, object)
+ */
+
+
+// primitive data type
+let name = "sajnus"; //string
+let id = 5810; //number (integer)
+let weight = 70.5; //number (floating)
+let isMarried = true; //boolean
+let area;
+let address = null;
+
+console.log(name, id, weight, isMarried, area, address);
---
+++ Milestone 3/Conceptual 1/variable.js
@@ -0,0 +1,19 @@
+// কোন কিছু print করার জন্য console.log("");
+console.log("Hello World");
+
+
+// variable এর নাম লেখার ধরণ
+var schoolRoll = 10; //camelCase
+var CollegeRoll = 20; //PascalCase
+var university_roll = 30; //snake_case
+
+
+// let and const
+// var বারবার একই নাম ব্যবহার করতে দেয়
+// let value update করতে দেয়, কিন্তু const দেয় না
+let age = 20;
+const name = "Sajnus";
+console.log(name, age);
+
+age = 25;
+console.log(age);
